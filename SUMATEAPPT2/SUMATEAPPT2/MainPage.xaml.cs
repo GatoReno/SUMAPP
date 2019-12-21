c7d4770839eeb94fae65c26ccf95d1e08f14b30b
EdRawr <erp.reno@hotmail.com>
2019-12-21 13:29:28 -0600

    Agregar archivos de proyecto.

---
+++ SUMATEAPPT2/SUMATEAPPT2/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using SUMATEAPPT2.Vista;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SUMATEAPPT2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnClick(object sender, EventArgs e)
+
+        {
+            await DisplayAlert("mi cabecera","mi mensaje","ok");
+             Application.Current.MainPage = new NavigationPage(new Page1());
+        }
+    }
+}
